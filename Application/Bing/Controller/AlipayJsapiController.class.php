1dd7d0dc137f4bec08b2ca08077a3a415fc918a8
wujindong <498298758@qq.com>
2017-09-16 19:52:11 +0800

    新增支付宝钱【包创建订单+JSAPI唤起收银台支付】

---
+++ Application/Bing/Controller/AlipayJsapiController.class.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * 使用本接口需要开通当面付
+ */
+namespace Bing\Controller;
+use Think\Controller;
+use Think\Log;
+
+class AlipayJsapiController extends Controller {
+
+
+    const GATEWAYURL = 'https://openapi.alipay.com/gateway.do';
+    const APPID = '2017080808092324';
+    const APIVESION = '1.0';
+    const POSTCHARSET = 'UTF-8';
+    const FORMAT = 'json';
+    private $privatekey='';
+    private $publickey='';
+
+    public function _initialize(){
+        //设置私钥和支付宝公钥（注：在服务器端生成公钥和私钥，把公钥上传到支付宝获取到支付宝公钥）
+        $this->privatekey="http://".$_SERVER['HTTP_HOST'].__ROOT__."/ThinkPHP/Library/Vendor/cbcalipay/aop/case/key/rsa_private_key.pem";
+        $this->publickey="http://".$_SERVER['HTTP_HOST'].__ROOT__."/ThinkPHP/Library/Vendor/cbcalipay/aop/case/key/alipay_public_key.pem";
+        vendor("cbcalipay.aop.AopClient");
+        vendor("cbcalipay.aop.request.AlipaySystemOauthTokenRequest");
+        vendor("cbcalipay.aop.request.AlipayTradeCreateRequest");
+    }
+
+    public function index() {
+        $redirect_url="http://".$_SERVER['HTTP_HOST'].U("Bing/AlipayJsapi/authorize");
+        $url="https://openauth.alipay.com/oauth2/publicAppAuthorize.htm?app_id=".self::APPID."&scope=auth_base&redirect_uri=".$redirect_url;
+        redirect($url);
+    }
+
+    /**
+     * 获取用户授权信息
+     */
+    public  function authorize(){
+        $aop = new \AopClient();
+        $aop->appId = self::APPID;
+        $aop->rsaPrivateKeyFilePath = $this->privatekey;
+        $aop->alipayPublicKey =$this->publickey;
+        $request = new \AlipaySystemOauthTokenRequest();
+        $request->setGrantType("authorization_code");
+        $request->setCode($_REQUEST['auth_code']);
+        $result = $aop->execute($request);
+        $data = (array) $result;
+        $oauth_token_response = (array) $data['alipay_system_oauth_token_response'];
+//        Log::write(var_export($oauth_token_response,true));
+        cookie("user_id",$oauth_token_response['user_id']);
+        $this->display("alipayJsapi/index");
+    }
+
+
+    /**
+     * 创建订单
+     * @param $buyer_id
+     */
+    public  function create_trade(){
+        $buyer_id=cookie("user_id");
+        $aop = new \AopClient();
+        $aop->appId = self::APPID;
+        $aop->rsaPrivateKeyFilePath = $this->privatekey;
+        $aop->alipayPublicKey = $this->publickey;
+        $request_create_trade = new \AlipayTradeCreateRequest();
+
+        $orderInfo = array(
+            'out_trade_no' => date("YmdHis"),
+            'total_amount' =>I('get.total_amount'),
+            'subject' =>I('get.subject'),
+            'buyer_id' =>$buyer_id
+        );
+        $request_create_trade->setBizContent(json_encode($orderInfo));
+        $results = (array)$aop->execute($request_create_trade);
+        Log::write(var_export($results,true));
+        $responseNode =(array)$results['alipay_trade_create_response'];
+//        Log::write(var_export($responseNode,true));
+        $data['code']=$responseNode['code'];
+        $data['msg']=$responseNode['sub_code'];
+        if (!empty($responseNode['code']) && $responseNode['code'] == 10000) {
+            //成功返预订单号
+            $data['trade_no']=$responseNode['trade_no'];
+            $this->ajaxReturn($data);
+        } else {
+            //失败返回先关状态
+           $this->ajaxReturn($data);
+        }
+    }
+}
