c4be44f53d37f6ea8ca3b185488c3c3587c577a8
wujindong <498298758@qq.com>
2017-09-16 19:55:50 +0800

    修改内容

--- Application/Bing/Controller/AlipayJsapiController.class.php
+++ Application/Bing/Controller/AlipayJsapiController.class.php
@@ -10,7 +10,7 @@ class AlipayJsapiController extends Controller {
 
 
     const GATEWAYURL = 'https://openapi.alipay.com/gateway.do';
-    const APPID = '2017080808092324';
+    const APPID = '';
     const APIVESION = '1.0';
     const POSTCHARSET = 'UTF-8';
     const FORMAT = 'json';
@@ -19,8 +19,8 @@ class AlipayJsapiController extends Controller {
 
     public function _initialize(){
         //设置私钥和支付宝公钥（注：在服务器端生成公钥和私钥，把公钥上传到支付宝获取到支付宝公钥）
-        $this->privatekey="http://".$_SERVER['HTTP_HOST'].__ROOT__."/ThinkPHP/Library/Vendor/cbcalipay/aop/case/key/rsa_private_key.pem";
-        $this->publickey="http://".$_SERVER['HTTP_HOST'].__ROOT__."/ThinkPHP/Library/Vendor/cbcalipay/aop/case/key/alipay_public_key.pem";
+        $this->privatekey="";
+        $this->publickey="";
         vendor("cbcalipay.aop.AopClient");
         vendor("cbcalipay.aop.request.AlipaySystemOauthTokenRequest");
         vendor("cbcalipay.aop.request.AlipayTradeCreateRequest");
