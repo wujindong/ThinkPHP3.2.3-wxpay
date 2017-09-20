026dc4e45af0d622a3ec4934c69de518c64bd740
wujindong <498298758@qq.com>
2017-09-20 09:25:24 +0800

    '新增微信支付公共类'

--- Application/Common/Controller/WxpayBaseController.class.php
+++ Application/Common/Controller/WxpayBaseController.class.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2017/9/20
- * Time: 9:14
- */
+namespace Common\Controller;
+use Think\Controller;
+class WxpayBaseController extends Controller{
+    public function _initialize(){
+        ini_set('date.timezone','Asia/Shanghai');
+        Vendor('weixin.WxPayApi');
+        Vendor('weixin.JsApiPay');
+        Vendor('weixin.WxPayNotify');
+        Vendor("weixin.NativePay");
+    }
+}
